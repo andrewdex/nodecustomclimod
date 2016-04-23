de537e1a421984ead73960edb5ce81eca95e88c9
Dilusha Gonagala <andrewdexis@gmail.com>
2016-04-23 17:22:32 +0530

    Commited Current After modularizing the project up to some extent

---
+++ core/clihandler.js
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+
+/**
+Name : NodeJS Custom CLI Module - Project Generator Template
+Author:  Dilusha Gonagala
+License : MIT
+Version : 1.0
+**/
+
+module.exports = {
+
+
+  listen: function() {
+    var generator = require("./generator.js");
+
+    //Slice out the array
+    var userArgs = process.argv.slice(2);
+
+    //Read Argument Values and Assign
+    var arg1 = userArgs[0],
+      arg2 = userArgs[1];
+
+    //Assign values received to the projecttype and projectname variables
+    var projecttype = arg1,
+      projectname = arg2;
+
+    //Pass two parameters of cli to the generator for processing
+    generator.generate(projecttype, projectname);
+
+
+  }
+
+
+
+
+}
---
+++ index.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+
+/**
+Name : NodeJS Custom CLI Module - Project Generator Template
+Author:  Dilusha Gonagala
+License : MIT
+Version : 1.0
+**/
+
+//Load Modules
+
+var clihandler = require("./core/clihandler");
+
+//Initialize CLI Handler
+clihandler.listen();
