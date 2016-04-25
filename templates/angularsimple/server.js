348bebdf82028ff9520b97b2bf85aea3f851ef65
Dilusha Gonagala <andrewdexis@gmail.com>
2016-04-25 12:42:06 +0530

    Express Commit

--- templates/angularsimple/server.js
+++ templates/angularsimple/server.js
@@ -1,15 +1,9 @@
 var express = require("express");
 var app = express();
 
-
-var port  	 = process.env.PORT || 5000; 				// set the port
-//var database = require('./config/database'); 			// load the database config
-
-// configuration ===============================================================
-//mongoose.connect(database.url); 	// connect to mongoDB database on modulus.io
+var port  	 = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + '/public'));
-
 
 app.listen(port);
 console.log("Your AngularSimple App Is up And Running on http://localhost:"+port+"");
